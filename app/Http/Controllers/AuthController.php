48e8a644a3ca6493498842fcdaee68941da24142
Mysaa-Abdulhai <mysaaabdulhai@gmail.com>
2022-06-05 19:42:36 +0300

    mysaa_1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,61 +5,107 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use App\Traits\ApiResponder;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
 class AuthController extends Controller
 {
+    use ApiResponder;
+
     public function register(Request $request){
-        $fields = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|string|unique:users,email',
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required|string',
+            'email'    => 'required|string|unique:users,email',
             'password' => 'required|string'
         ]);
 
-        $user = User::create([
-            'name' => $fields['name'],
-            'email' => $fields['email'],
+        if ($validator->fails())
+        return $this->errorResponse($validator->getMessageBag());
+
+        $fields = $request->validate([
+            'name'     => 'required|string',
+            'email'    => 'required|string|unique:users,email',
+            'password' => 'required|string'
+        ]);
+
+        $user1 = new User();
+        $user1->name = $request->name;
+        $user1->description = $request->description ?? '';
+        $user1->email = $request->email;
+        $user1->password = bcrypt($request->password);
+        $user1->save();
+
+        $user1 = User::create([
+            'name'     => $fields['name'],
+            'email'    => $fields['email'],
             'password' => bcrypt($fields['password'])
         ]);
 
-        $token = $user->createToken('myapptoken')->plainTextToken;
+        $token = $user1->createToken('myapptoken')->plainTextToken;
 
         $response = [
-            'user' => $user,
+            'user'  => $user1,
             'token' => $token
         ];
 
-        return response($response, 201);
+        return $this->createdResponse($response,'User Register Successfully');
+        // return response($response, 201);
     }
+
     public function login(Request $request) {
-        $fields = $request->validate([
-            'email' => 'required|string',
+
+        // Validation
+        $login_data = Validator::make($request->all(), [
+            'email'    => 'required|string',
             'password' => 'required|string'
         ]);
 
-        // Check email
-        $user = User::where('email', $fields['email'])->first();
-
-        // Check password
-        if(!$user || !Hash::check($fields['password'], $user->password)) {
-            return response([
-                'message' => 'Bad creds'
-            ], 401);
+        if ($login_data->fails()) {
+            return $this->errorResponse($login_data->errors()->getMessages());
         }
+
+        // $fields = $request->validate([
+        //     'email' => 'required|string',
+        //     'password' => 'required|string'
+        // ]);
+
+        // Check email
+        $user = User::where('email', $request->email)->first();
+
+
+        // validate User data
+        try {
+            if (!auth()->attempt($login_data->validated())) {
+                return $this->forbiddenResponse('Invalid Credentials');
+            }
+        } catch (ValidationException $e) {
+            return $this->notFoundResponse('Bad creds');
+        }
+
+        // // Check password
+        // if(!$user || !Hash::check($fields['password'], $user->password)) {
+        //     return response([
+        //         'message' => 'Bad creds'
+        //     ], 401);
+        // }
 
         $token = $user->createToken('myapptoken')->plainTextToken;
 
         $response = [
-            'user' => $user,
+            'user'  => $user,
             'token' => $token
         ];
 
-        return response($response, 201);
+        return $this-> okResponse($response,'User logged in Successfully');
     }
 
     public function logout(Request $request) {
         auth()->user()->tokens()->delete();
 
-        return [
-            'message' => 'Logged out'
-        ];
+        return $this->noContentResponse('Logged out');
+        // return [
+        //     'message' => 'Logged out'
+        // ];
     }
 }
---
+++ app/Traits/ApiResponder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+trait ApiResponder
+{
+    public function successResponse(mixed $data, $message, int $statusCode = Response::HTTP_OK): JsonResponse
+    {
+        if (!$message) {
+            $message = Response::$statusTexts[$statusCode];
+        }
+
+        return response()->json
+            ([
+                'status_code' => $statusCode,
+                'message' => $message,
+                'data' => $data
+            ]);
+    }
+
+    public function errorResponse($message = '', int $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR): JsonResponse
+    {
+        if (!$message) {
+            $message = Response::$statusTexts[$statusCode];
+        }
+
+        return response()->json
+        ([
+            'status_code' => $statusCode,
+            'message' => ['errors' => $message],
+            'data' => null
+        ]);
+    }
+
+    public function okResponse(mixed $data, $message = ''): JsonResponse
+    {
+        return $this->successResponse($data, $message);
+    }
+
+    public function createdResponse($data, $message = ''): JsonResponse
+    {
+        return $this->successResponse($data, $message, Response::HTTP_CREATED);
+    }
+
+    public function noContentResponse(string $message = ''): JsonResponse
+    {
+        return $this->successResponse([], $message,Response::HTTP_NO_CONTENT);
+    }
+
+    public function badRequestResponse(string $message = ''): JsonResponse
+    {
+        return $this->errorResponse($message, Response::HTTP_BAD_REQUEST);
+    }
+
+    public function unauthorizedResponse(string $message = ''): JsonResponse
+    {
+        return $this->errorResponse($message, Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function forbiddenResponse(string $message = ''): JsonResponse
+    {
+        return $this->errorResponse($message, Response::HTTP_FORBIDDEN);
+    }
+
+    public function notFoundResponse(string $message = ''): JsonResponse
+    {
+        return $this->errorResponse($message, Response::HTTP_NOT_FOUND);
+    }
+
+    public function conflictResponse(string $message = ''): JsonResponse
+    {
+        return $this->errorResponse($message, Response::HTTP_CONFLICT);
+    }
+
+    public function unprocessableResponse(string $message = ''): JsonResponse
+    {
+        return $this->errorResponse($message, Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}

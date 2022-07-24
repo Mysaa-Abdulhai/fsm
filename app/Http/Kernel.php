9e5ace417c566bfdcfee9f6d641bf54c2e6f80c7
Joseph Alsaka <joseph70.alsaka@gmail.com>
2022-07-24 19:12:28 +0300

    joseph_44

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'verified' => \App\Http\Middleware\Verified::class,
         'haveProfile' => \App\Http\Middleware\HaveProfile::class,
         'oneProfile' => \App\Http\Middleware\OneProfile::class,
         'acceptPermission' => \App\Http\Middleware\AcceptPermission::class,
